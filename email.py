4cca8e070d1ac0358fd9dfe23a5a96461dad34a8
Noah Dinan <91911303+shinysocks@users.noreply.github.com>
2022-04-30 16:04:53 -0500

    Add files via upload

---
+++ email.py
@@ -0,0 +1,8 @@
+# open email text file and put it into a variable
+with open('ShoutbombMarch2022.txt', 'r') as f:
+    msg_lines = f.readlines()
+
+print(msg_lines)
+
+
+# https://datatofish.com/convert-text-file-to-csv-using-python-tool-included/  
