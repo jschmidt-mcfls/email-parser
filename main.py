51ca685dade1f21204bb0acbbbb6839f50ca4d86
Noah Dinan <91911303+shinysocks@users.noreply.github.com>
2022-05-17 21:27:41 +0000

    added total parsing

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Imports
 from xlwt import Workbook
 from time import sleep
 from os import system
@@ -21,81 +20,85 @@ for _ in range(0, 3):
 # Try to import file
 found = False
 while not found:
-    filename = "ShoutbombApril2022.txt"
+    filename = "ShoutbombMarch2022.txt"
     try:
-        with open(filename, 'r') as f:
+        with open(filename, "r") as f:
             email = f.read()
         found = True
     except FileNotFoundError:
         print("File not found...")
 
 # Variables
-new_filename = filename.replace(".txt", "")
-queries = {
-    'Hold notices sent for the month': 0,
-    'Hold cancel notices sent for the month': 0,
-    'Overdue notices sent for the month': 0,
-    'Overdue items eligible for renewal, notices sent for the month': 0,
-    'Overdue items ineligible for renewal, notices sent for the month': 0,
-    'Overdue items renewed successfully by patrons for the month': 0,
-    'Overdue items unsuccessfully renewed by patrons for the month': 0,
-    'Renewal notices sent for the month': 0,
-    'Items eligible for renewal notices sent for the month': 0,
-    'Items ineligible for renewal notices sent for the month': 0,
-    'Items renewed successfully by patrons for the month': 0,
-    'Items unsuccessfully renewed by patrons for the month': 0,
-    }
+newFilename = filename.replace(".txt", "")
+queries = [
+    "Hold notices sent for the month",
+    "Hold cancel notices sent for the month",
+    "Overdue notices sent for the month",
+    "Overdue items eligible for renewal, notices sent for the month",
+    "Overdue items ineligible for renewal, notices sent for the month",
+    "Overdue items renewed successfully by patrons for the month",
+    "Overdue items unsuccessfully renewed by patrons for the month",
+    "Renewal notices sent for the month",
+    "Items eligible for renewal notices sent for the month",
+    "Items ineligible for renewal notices sent for the month",
+    "Items renewed successfully by patrons for the month",
+    "Items unsuccessfully renewed by patrons for the month",
+    "Totals?"
+    ]
 
 libraries = [
-    'Atkinson', 'Bay View', 'Villard', 'Wash Park', 'Capitol',
-    'Mitchell St.', 'Zablocki', 'Center St.',
-    'Hales Corners', 'Whitefish Bay', 'Shorewood', 'Cudahy',
-    'North Shore', 'Brown Deer', 'Tippecanoe', 'St. Francis',
-    'Good Hope', 'West Allis', 'Wauwatosa', 'Oak Creek',
-    'West Milwaukee', 'King', 'Greendale', 'Greenfield',
-    'East', 'South Milwaukee', 'Franklin', 'Central'
+    "Atkinson", "Bay View", "Villard", "Wash Park", "Capitol",
+    "Mitchell St.", "Zablocki", "Center St.",
+    "Hales Corners", "Whitefish Bay", "Shorewood", "Cudahy",
+    "North Shore", "Brown Deer", "Tippecanoe", "St. Francis",
+    "Good Hope", "West Allis", "Wauwatosa", "Oak Creek",
+    "West Milwaukee", "King", "Greendale", "Greenfield",
+    "East", "South Milwaukee", "Franklin", "Central"
     ]
 
 workbook = Workbook()
 
-# rename
-totalsByBranch = workbook.add_sheet(f"Totals by Branch {new_filename.replace('Shoutbomb', '')}")
-totals = workbook.add_sheet(f"Totals {new_filename.replace('Shoutbomb', '')}")
+splittedEmail = email.split("=TOTALS BY BRANCH=")[0]
+
+totalsByBranch = workbook.add_sheet(f"Totals by Branch {newFilename.replace('Shoutbomb', '')}")
+totalUsers = workbook.add_sheet("Total Registered Users")
 
 
 # Import dictionary data to Totals by Branch sheet
-def firstSheet():
-    global email
-    email = email.split("=TOTALS=")[0]
-    queryKeys = list(queries.keys())
-    for key in queries.keys():
-        totalsByBranch.write(queryKeys.index(key)+1, 0, key)
-    for branch in email.split('Branch:: '):
-        for library in libraries:
-            if library in branch:
-                y_pos = libraries.index(library)+1
-                totalsByBranch.write(0, y_pos, library)
-                new_queries = queries.copy()
-                for line in branch.splitlines():
-                    for key in new_queries:
-                        if key in line:
-                            new_line = line.replace(key, '')
-                            new_line = new_line.replace(' = ', '')
-                            line = new_line
-                            new_queries[key] = int(line)
+def parse(what):
+    valuesList = []
+    for line in what.splitlines():
+        for key in queries.copy():
+            if key in line:
+                newLine = line.replace(key, "")
+                newLine = newLine.replace(" = ", "")
+                valuesList.append(int(newLine))
+    return valuesList
+    
+# First Sheet
+email_text = splittedEmail.split("=TOTALS=")[0]
+for query in queries:
+    totalsByBranch.write(int(queries.index(query)+1), 0, query)
+row = 0
+column = 0
+for branch in email_text.split("Branch:: "):
+    for library in libraries:
+        row = 0
+        if library in branch:
+            column += 1
+            totalsByBranch.write(0, column, library)
+            libQueries = parse(branch)
+            for query in libQueries:
+                row += 1
+                totalsByBranch.write(row, column, query)
 
-                for query in list(new_queries.keys()):
-                    x_pos = list(new_queries.keys()).index(query)
-                    totalsByBranch.write(x_pos+1, y_pos, new_queries[query])
-
-
-def secondSheet():
-    pass
-
-
-# Run functions
-firstSheet()
-secondSheet()
+row = 0 
+column += 1
+totals = parse(splittedEmail.split("=TOTALS=")[1])
+totalsByBranch.write(row, column, "Totals")
+for query in totals:
+    row += 1
+    totalsByBranch.write(row, column, query)
 
 # Save workbook
 workbook.save(filename.replace(".txt", ".xls"))
